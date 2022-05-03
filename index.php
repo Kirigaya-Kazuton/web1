60ef1e8e3529af4887c784619a403d77197144f8
kirito <thiagodossantos315@hotmail.com>
2022-05-02 22:33:48 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+require "./src/server.php";
+require "./src/authenticate.php";
+
+$error = false;
+$password = $email = "";
+
+if (!$login && $_SERVER["REQUEST_METHOD"] == "POST") {
+  if (isset($_POST["email"]) && isset($_POST["password"])) {
+
+    $conn = connect_db();
+
+    $email = mysqli_real_escape_string($conn,$_POST["email"]);
+    $password = mysqli_real_escape_string($conn,$_POST["password"]);
+    $password = md5($password);
+
+    $sql = "SELECT id,nome,email,senha FROM $table_users
+            WHERE email = '$email';";
+
+    $result = mysqli_query($conn, $sql);
+    if($result){
+      if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+
+        if ($user["senha"] == $password) {
+
+          $_SESSION["user_id"] = $user["id"];
+          $_SESSION["user_name"] = $user["name"];
+          $_SESSION["user_email"] = $user["email"];
+
+          header("Location: " . dirname($_SERVER['SCRIPT_NAME']) . "./src/worlde.php");
+          exit();
+        }
+        else {
+          $error_msg = "Senha incorreta!";
+          $error = true;
+        }
+      }
+      else{
+        $error_msg = "Usuário não encontrado!";
+        $error = true;
+      }
+    }
+    else {
+      $error_msg = mysqli_error($conn);
+      $error = true;
+    }
+  }
+  else {
+    $error_msg = "Por favor, preencha todos os dados.";
+    $error = true;
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./src/components/Login/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+    <h1>Login</h1>
+    <?php if ($login): ?>
+    <h3>Você já está logado!</h3>
+    <?php exit(); ?>
+    <?php endif; ?>
+
+    <?php if ($error): ?>
+    <h3 style="color:red;"><?php echo $error_msg; ?></h3>
+    <?php endif; ?>
+       <form action="index.php" method="post">
+           <label for="email" id="labell">Email</label>
+           <input type="text" name="email" id="email" placeholder="Digite seu e-mail" value="<?php echo $email; ?>">
+           <label for="password " id="labelll">Senha</label>
+           <i class="fa fa-eye" aria-hidden="true"></i>
+           <input type="password" name="password" id="password" placeholder="Digite sua senha">
+           <input type="submit" value="Login" id="btn">
+           <p>Não Tem uma conta?
+                <a href="src\Register.php">Cadastre-se</a>
+           </p> 
+       </form>
+    </div>
+</body>
+</html>
